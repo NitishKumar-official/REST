1884c818e8a8a0302cb9bb767cfdfafdb80336fd
nitish <kumarnitish27212@gmail.com>
2024-06-11 19:47:43 +0530

    rest apis

---
+++ rest.js
@@ -0,0 +1,22 @@
+/* REST(Representational state transfer)
+=> Rest is an architectural style that defines a set of constraints to be used for creating web services
+
+CRUD operation
+
+1.GET   =>  retrives resources
+2.POST  =>  submits new data to the server
+3.PUT   =>  updates existin data
+4.PATCH =>  UPDATE IXISTING DATA PARTIALLY
+5.DELETE=>  REMOVES DATA
+
+
+
+CREATING RESTFUL APIs
+
+GET        /posts            to get dat for all posts
+POST       /posts            to add a new post
+GET        /posts/:id        to get one post(using id)
+PATCH      /posts/:id        to update specific post
+DELETE     /posts/:id        to delete specific post
+
+*/
